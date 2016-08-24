c23643d06de6ef2218cac015773b08dd7c07b9cc
liberal-boy <liberal-boy@protonmail.com>
2016-08-24 22:29:35 +0800

    Added about to settings

--- app/src/main/kotlin/com/v2ray/actinium/ui/SettingsActivity.kt
+++ app/src/main/kotlin/com/v2ray/actinium/ui/SettingsActivity.kt
@@ -1,19 +1,27 @@
 package com.v2ray.actinium.ui
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Build
 import android.os.Bundle
 import android.preference.CheckBoxPreference
 import android.preference.Preference
 import android.preference.PreferenceFragment
+import android.support.v7.app.AppCompatActivity
 import com.v2ray.actinium.R
 import com.v2ray.actinium.service.V2RayService
+import de.psdev.licensesdialog.LicensesDialogFragment
+import org.jetbrains.anko.act
 import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
 
 class SettingsActivity : BaseActivity() {
     companion object {
         const val PREF_START_ON_BOOT = "pref_start_on_boot"
         const val PREF_BLACKLIST = "pref_blacklist"
         const val PREF_EDIT_BLACKLIST = "pref_edit_blacklist"
+        const val PREF_LICENSES = "pref_licenses"
+        const val PREF_DONATE = "pref_donate"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,10 +34,29 @@ class SettingsActivity : BaseActivity() {
     class SettingsFragment : PreferenceFragment() {
         val blacklist by lazy { findPreference(PREF_BLACKLIST) as CheckBoxPreference }
         val editBlacklist: Preference by lazy { findPreference(PREF_EDIT_BLACKLIST) }
+        val licenses: Preference by lazy { findPreference(PREF_LICENSES) }
+        val donate: Preference by lazy { findPreference(PREF_DONATE) }
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             addPreferencesFromResource(R.xml.pref_settings)
+
+            licenses.setOnPreferenceClickListener {
+                val fragment = LicensesDialogFragment.Builder(act)
+                        .setNotices(R.raw.licenses)
+                        .setIncludeOwnLicense(false)
+                        .build()
+                fragment.show((act as AppCompatActivity).supportFragmentManager, null)
+                true
+            }
+
+            donate.setOnPreferenceClickListener {
+                val clipboard = act.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
+                val clip = ClipData.newPlainText("BTC", "191Ky8kA4BemiG3RfPiJjStEUqFcQ4DdAB")
+                clipboard.primaryClip = clip
+                toast(R.string.toast_copied_to_clipboard)
+                true
+            }
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 V2RayService.checkStatusEvent {
